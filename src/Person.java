57f3205fac0e4735adcbf996225412e7563bc2d5
unknown <sambitmishra987@gmail.com>
2016-10-09 00:47:43 +0530

    Adding person class

---
+++ src/Person.java
@@ -0,0 +1,17 @@
+public class Person{
+	private String fname;
+	private String lname;
+
+	public Person(String fname, String lname){
+		this.fname = fname;
+		this.lname = lname;
+	}
+
+	public void setFirstName(String fname){
+		this.fname = fname;
+	}
+
+	public void setLastName(String lname){
+		this.lname = lname;
+	}
+}
