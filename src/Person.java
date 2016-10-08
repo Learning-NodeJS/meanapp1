61a33dd0bf7514ca0389216bde3856d7782762ff
unknown <sambitmishra987@gmail.com>
2016-10-09 00:58:41 +0530

    Adding Age to Person.

--- src/Person.java
+++ src/Person.java
@@ -1,10 +1,11 @@
 public class Person{
 	private String fname;
 	private String lname;
-
-	public Person(String fname, String lname){
+	private int age;
+	public Person(String fname, String lname, int age){
 		this.fname = fname;
 		this.lname = lname;
+		this.age = age;
 	}
 
 	public void setFirstName(String fname){
